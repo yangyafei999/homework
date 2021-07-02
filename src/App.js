e36a3a69935c9d4698795648552a1ca26d1df454
yangyafei999 <1057301319@qq.com>
2021-07-02 16:07:15 +0800

    uuu

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://baike.baidu.com/item/%E5%BA%86%E7%A5%9D%E4%B8%AD%E5%9B%BD%E5%85%B1%E4%BA%A7%E5%85%9A%E6%88%90%E7%AB%8B100%E5%91%A8%E5%B9%B4/55757846?fr=aladdin"
           rel="noopener noreferrer"
         >
-          加油！
+          加油
         </a>
       </header>
     </div>
